6c35d648ab11046b5b814c88e71cf58537a04313
zuyuan.zhu <zuyuan.zhu@beko.com>
2024-03-26 14:08:30 +0800

    log pose to files

--- src/pose_accuracy.py
+++ src/pose_accuracy.py
@@ -1,3 +1,4 @@
+import os
 import rclpy
 from rclpy.node import Node
 from rclpy.timer import Timer
@@ -6,12 +7,22 @@ from rclpy.logging import LoggingSeverity
 from nav_msgs.msg import Odometry
 from rvc_msgs.msg import RvcOdom
 from math import sqrt
+from datetime import datetime
 
 
 class PoseComparisonNode(Node):
     def __init__(self):
         super().__init__('pose_comparison_node')
 
+        self.file_path = os.path.join(os.environ['HOME'], 'ros2_ws', 'mapping')
+        # Ensure the directory exists
+        if not os.path.exists(self.file_path):
+            os.makedirs(self.file_path)
+
+        # Generate a timestamp when the node is initialized
+        self.current_time_str = datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
+
+        self.pose_rvc_odom_time = None
         qos_reliable = QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT)
         # If the publisher uses BEST_EFFORT, then set reliability to ReliabilityPolicy.BEST_EFFORT
         
@@ -46,14 +57,17 @@ class PoseComparisonNode(Node):
 
     def callback_rvc_odom(self, msg):
         self.pose_rvc_odom = msg.rvc_pose
+        self.pose_rvc_odom_time = datetime.now()
         self.get_logger().debug(f"Received pose on /rvc_odom: {msg.rvc_pose}")
 
     def callback_custom_odom(self, msg):
         self.pose_custom_odom = msg.pose.pose
+        self.pose_custom_odom_time = datetime.now()
         self.get_logger().debug(f"Received pose on /custom_odom: {msg.pose.pose}")
 
     def callback_odom(self, msg):
         self.pose_odom = msg.pose.pose
+        self.pose_odom_time = datetime.now()
         self.get_logger().debug(f"Received pose on /odom: {msg.pose.pose}")
 
     def calculate_distance(self, pose1, pose2):
@@ -63,13 +77,46 @@ class PoseComparisonNode(Node):
         self.get_logger().debug(f'Calculating distance: pose1.y = {pose1.y}, pose2.y = {pose2.position.y}')
         return sqrt(dx*dx + dy*dy)
 
+    # def compare_poses(self):
+    #     if self.pose_rvc_odom and self.pose_odom:
+    #         discrepancy_rvc = self.calculate_distance(self.pose_rvc_odom, self.pose_odom)
+    #         self.get_logger().info('Discrepancy between /rvc_odom and /odom: %f' % discrepancy_rvc)
+    #         with open('/home/zuyuan/ros2_ws/maping/discrepancy_rvc.txt', 'a') as file:
+    #             file.write(f'{discrepancy_rvc}\n')
+    #     if self.pose_rvc_odom and self.pose_custom_odom:
+    #         discrepancy_custom = self.calculate_distance(self.pose_rvc_odom, self.pose_custom_odom)
+    #         self.get_logger().info('Discrepancy between /rvc_odom and /custom_odom: %f' % discrepancy_custom)
+    #         with open('/home/zuyuan/ros2_ws/maping/discrepancy_custom.txt', 'a') as file:
+    #             file.write(f'{discrepancy_custom}\n')
+    #     else:
+    #         self.get_logger().debug("Waiting for all poses to be received...")
+
     def compare_poses(self):
+        file_name_rvc = f'discrepancy_rvc_{self.current_time_str}.txt'
+        file_path_rvc = os.path.join(self.file_path, file_name_rvc)
+
+        # Construct the file path dynamically
         if self.pose_rvc_odom and self.pose_odom:
+            timestamp_str = self.pose_rvc_odom_time.strftime('%Y-%m-%d %H:%M:%S.%f')       
             discrepancy_rvc = self.calculate_distance(self.pose_rvc_odom, self.pose_odom)
-            self.get_logger().info('Discrepancy between /rvc_odom and /odom: %f' % discrepancy_rvc)
-        elif self.pose_rvc_odom and self.pose_custom_odom:
+            self.get_logger().info(f'Discrepancy between /rvc_odom and /odom: {discrepancy_rvc}')
+            
+            file_name_rvc = f'discrepancy_rvc_{self.current_time_str}.txt'
+            file_path_rvc = os.path.join(self.file_path, file_name_rvc)
+            with open(file_path_rvc, 'a') as file:
+                file.write(f'{timestamp_str}, {discrepancy_rvc}\n')
+
+        if self.pose_rvc_odom and self.pose_custom_odom:
+            timestamp_str = self.pose_rvc_odom_time.strftime('%Y-%m-%d %H:%M:%S.%f')
             discrepancy_custom = self.calculate_distance(self.pose_rvc_odom, self.pose_custom_odom)
-            self.get_logger().info('Discrepancy between /rvc_odom and /custom_odom: %f' % discrepancy_custom)
+            self.get_logger().info(f'Discrepancy between /rvc_odom and /custom_odom: {discrepancy_custom}')
+            
+            file_name_custom = f'discrepancy_custom_{self.current_time_str}.txt'
+            file_path_custom = os.path.join(self.file_path, file_name_custom)
+            if not os.path.exists(file_path):
+                os.makedirs(file_path)
+            with open(file_path_custom, 'a') as file:
+                file.write(f'{timestamp_str}, {discrepancy_custom}\n')
         else:
             self.get_logger().debug("Waiting for all poses to be received...")
 
